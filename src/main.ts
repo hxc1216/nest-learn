6e21990d480caf4bd339628e2cff5ff39fd89ff3
HouXinchao <860618589@qq.com>
2023-06-19 17:47:11 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,50 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+// 使用 FastifyAdapter 替换默认的 Express
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+// 版本配置
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
+
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionsFilter } from './common/exceptions/http.exception.filter';
+
+import { generateDocument } from './doc';
+
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // 统一响应体格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  // 异常过滤器
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionsFilter());
+
+  // 接口版本化管理
+  app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'], // 版本设置 VERSION_NEUTRAL:任何版本或者没有版本
+    type: VersioningType.URI,
+  });
+
+  // 创建文档
+  generateDocument(app);
+
+  // 开启HMR热更新
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
+  await app.listen(3000);
+}
+bootstrap();
---
+++ src/user/user.controller.ts
@@ -0,0 +1,76 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  VERSION_NEUTRAL,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { BusinessException } from 'src/common/exceptions/business.exception';
+import { ConfigService } from '@nestjs/config';
+
+@Controller({
+  path: 'user',
+  version: [VERSION_NEUTRAL],
+})
+export class UserController {
+  constructor(
+    private readonly userService: UserService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get('getTestName')
+  getTestName() {
+    return this.configService.get('TEST_VALUE').name;
+  }
+
+  @Get('findError')
+  findError() {
+    const a: any = {};
+    console.log(a.b.c);
+
+    return this.userService.findAll();
+  }
+
+  @Get('findBusError')
+  findBusError() {
+    const a: any = {};
+    try {
+      console.log(a.b.c);
+    } catch (error) {
+      throw new BusinessException('参数错误');
+    }
+
+    return this.userService.findAll();
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
